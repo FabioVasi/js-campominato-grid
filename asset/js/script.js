14a0c134f6cab9a8022043fb7d724c7d81c8c2cd
FabioVasi <vasifabio96@gmail.com>
2023-09-01 14:53:10 +0200

    add css structure

--- asset/js/script.js
+++ asset/js/script.js
@@ -16,3 +16,9 @@ Di cosa ho bisogno per generare i numeri?
 Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 Le validazioni e i controlli possiamo farli anche in un secondo momento.
  */
+
+
+const maxSquare = 100;
+
+const squareEl = document.querySelector('.square');
+
