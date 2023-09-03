447f4a2aab67209ccff23af6a2c4a67dacdc91c5
FabioVasi <vasifabio96@gmail.com>
2023-09-03 21:59:01 +0200

    try to add function

--- asset/js/script.js
+++ asset/js/script.js
@@ -29,34 +29,44 @@ Strumenti.
 - createElement
  */
 
+
+
+
+function generateGrill(domElement, maxSquare) {
+    console.log(this);
+    // creo un ciclo che produca automaticamente i quadrati di cui ho bisogno
+    for (let i = 0; i < maxSquare; i++) {
+        // creo l'elemento HTML che costituirà i miei quadrati
+        const elementSquare = document.createElement('div');
+        // do una classev all'elemento creato
+        elementSquare.className = 'square'
+        // appendo gli elementi creati per poterli visualizzare
+        elementSquare.append(i + 1);
+   
+        domElement.append(elementSquare)
+        // creo un evento che si attivi ad un click dentro un quadrato qualunque
+        elementSquare.addEventListener('click', function() {
+
+            elementSquare.classList.toggle('bg-skyblue')
+
+            console.log('ho cliccato sulla cella ' + i);
+
+        })
+
+    }
+
+}
 // decido quanti quadrati voglio in pagina
 const maxSquare = 100;
 // seleziono un elemento da HTML nel quale vooro visualizzare la griglia
 const squareEl = document.querySelector('.row');
 
+generateGrill(squareEl, maxSquare)
+
 // creo un pulsante che se cliccato produrrà la griglia
 document.getElementById('produce').addEventListener('click', function() {
 
-    // creo un ciclo che produca automaticamente i quadrati di cui ho bisogno
-    for (let i = 0; i < maxSquare; i++) {
-    // creo l'elemento HTML che costituirà i miei quadrati
-       const elementSquare = document.createElement('div');
-   // do una classev all'elemento creato
-       elementSquare.className = 'square'
-   // appendo gli elementi creati per poterli visualizzare
-       elementSquare.append(i + 1);
-       
-       squareEl.append(elementSquare)
-   // creo un evento che si attivi ad un click dentro un quadrato qualunque
-       elementSquare.addEventListener('click', function() {
-   
-           elementSquare.classList.toggle('bg-skyblue')
-   
-           console.log('ho cliccato sulla cella ' + i);
-   
-       })
-
-    }
+    generateGrill(domElement, maxSquare)
 
 })
 
